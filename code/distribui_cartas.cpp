226f58f6f539ecb76b9c2736ceeff50da173555e
Janduigorila <109387680+Janduigorila@users.noreply.github.com>
2023-05-20 15:37:57 -0300

    inicio da função distribui cartas

--- code/distribui_cartas.cpp
+++ code/distribui_cartas.cpp
@@ -1,13 +1,19 @@
 #include "../header/embaralhamento.hpp"
 #include "../header/jogo.hpp"
+#include "../header/dupla.hpp"
+#include "../header/cartas.hpp"
 #include "../header/embaralhamento.hpp"
 #include <vector>
 #include <iostream>
 
 std::vector<Carta> distribui_cartas(){
     Baralho baralho;
+    baralho.inicializa_Baralho();
     embaralhar(baralho);
-    std::vector<Carta> cartas;
+    std::vector<Carta> mao;
+    while(mao.size() <= 4){
+        
+    }
     
 
     }
