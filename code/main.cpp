261846799ad4c19a40eb4a8bc2e5b89d731b1931
gvcf20 <gabrielcruzeiro.ferreira@gmail.com>
2023-05-29 18:28:06 -0300

    Criei o laço na main que irá criar jogos até que uma dupla tenha vencido

--- code/main.cpp
+++ code/main.cpp
@@ -22,12 +22,22 @@ int main(){
         std::pair<Dupla, Dupla> duplas = set_duplas(jogadores);
         exibe_duplas(duplas);
 
-        JOGO jogo;
-        while(duplas.first.pontuacao < 12){
-            jogo.comeca_rodada(duplas);
-            duplas.first.pontuacao +=2;
-        };
 
+        while(duplas.first.jogos_vencidos < 2 && duplas.second.jogos_vencidos < 2){
+            JOGO jogo;
+            jogo.comeca_rodada(duplas);
+
+        }
+
+        if(duplas.first.jogos_vencidos = 2){
+            std::cout << "Os Vencedores são: " << duplas.first.duplinha.first.nome_jogador
+            << " e "
+            << duplas.first.duplinha.second.nome_jogador << std::endl;
+        } else{
+            std::cout << "Os Vencedores são: " << duplas.second.duplinha.first.nome_jogador
+            << " e "
+            << duplas.second.duplinha.second.nome_jogador << std::endl;
+        }
     }
 
     return 0;
