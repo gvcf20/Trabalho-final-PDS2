ea36172af4924e4d4c48122b0efe4e5e1bfb36d9
gvcf20 <gabrielcruzeiro.ferreira@gmail.com>
2023-06-05 18:22:38 -0300

    tentando debugar

--- code/rodada.cpp
+++ code/rodada.cpp
@@ -71,7 +71,8 @@ void Comeca_Rodada::comeca_rodada(std::pair <Dupla, Dupla>& duplas_h){
         rodada.exibe_cartas(duplas_h);
         std::vector<Carta> cartas_jogadas;
         cartas_jogadas = sub_rodada.joga_carta(duplas_h, indice);
-        std::vector<Carta>maior_carta = regras.maior_carta(cartas_jogadas);
+        std::vector<Carta>maior_carta;
+        maior_carta = regras.maior_carta(cartas_jogadas);
 
         for(size_t i = 0; i < maior_carta.size(); ++i){
             std::cout << "A maior carta foi " <<maior_carta[i].toString() << std::endl;
