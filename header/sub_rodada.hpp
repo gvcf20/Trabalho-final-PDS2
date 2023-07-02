48f7af2005a8d4adb8b0edbfcaf974eed4e0a658
gvcf20 <gabrielcruzeiro.ferreira@gmail.com>
2023-07-02 15:32:34 -0300

    testes

--- header/sub_rodada.hpp
+++ header/sub_rodada.hpp
@@ -17,14 +17,6 @@ class Sub_Rodada{
     unsigned pontuacao_sub_rodada_dupla1;
     unsigned pontuacao_sub_rodada_dupla2;
 };
-//Este método estará responsável por avaliar as sub_partidas. Nesse sentido, sub_partidas
-//seriam relacionadas ao lance em que cada jogador joga suas cartas e avaliamos quem ganhou
-//temos cinco situações possiveis: uma dupla vence a primeira e a segunda;
-//Uma dupla ganha a primera e empata a segunda;
-//Uma dupla empata a primeira e ganha na segunda;
-//uma dupla ganha na primeira, perde na segunda e ganha na terceira
-//Uma dupla ganha na primeira, perde na segunda e empata na terceira e o peso da primeira
-//sub_rodada é maior que as demais
 
 
 
